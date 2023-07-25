e72c2639e01613b4ed69c84e263c985c6838a028
Wytamma Wirth <wytamma.wirth@me.com>
2023-07-25 14:15:02 +1000

    :sparkles: QC column

--- src/austrakka_covid_tree/workflow/scripts/format_nextclade.py
+++ src/austrakka_covid_tree/workflow/scripts/format_nextclade.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv(snakemake.input.nextclade_tsv, sep='\t')
+COVERGE_THRESHOLD = 0.9
+
+df = pd.read_csv(snakemake.input.nextclade_tsv, sep="\t")
+
+# rename columns for AT proforma
 df = df.rename(columns={
     "seqName": "Seq_ID",
     "clade": "Clade",
@@ -9,5 +13,8 @@ df = df.rename(columns={
     "qc.overallScore": "NC_qc.overallScore",
     "qc.overallStatus": "NC_qc.overallStatus",
 })
+
+# Set df QC to FAIL if coverage is less than COVERGE_THRESHOLD otherwise set to PASS
+df["QC"] = df.apply(lambda x: "FAIL" if x["Coverage"] < COVERGE_THRESHOLD else "PASS", axis=1)
 df.drop(columns=["index"], inplace=True)
-df.to_csv(snakemake.output.formated_tsv, sep='\t', index=False)
+df.to_csv(snakemake.output.formated_tsv, sep="\t", index=False)
