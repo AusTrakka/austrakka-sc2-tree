21e3267eddb1d81d7934fb605e62712e5701f162
Wytamma Wirth <wytamma.wirth@me.com>
2023-07-25 14:16:12 +1000

    :art: mask low coverage then unassigned

--- src/austrakka_covid_tree/workflow/scripts/mask_lineages.py
+++ src/austrakka_covid_tree/workflow/scripts/mask_lineages.py
@@ -3,9 +3,11 @@
 import pandas as pd
 
 df = pd.read_csv(snakemake.input.nextclade_tsv, sep="\t")
-df["qc.overallStatus"] = df["qc.overallStatus"].fillna("bad")
-df.loc[df["qc.overallStatus"] == "bad", "Nextclade_pango"] = "Unassigned"
+
 df.coverage = df.coverage.fillna(0)
 df.coverage = df.coverage * 100
 df.loc[df.coverage < 90, "Nextclade_pango"] = "LowCoverage"
+
+df["qc.overallStatus"] = df["qc.overallStatus"].fillna("bad")
+df.loc[df["qc.overallStatus"] == "bad", "Nextclade_pango"] = "Unassigned"
 df.to_csv(snakemake.output.masked_nextclade_tsv, sep="\t", index=False)
