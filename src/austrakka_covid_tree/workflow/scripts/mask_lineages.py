8012d44d23c155109417a0837795d983e53174d5
Wytamma Wirth <wytamma.wirth@me.com>
2023-07-28 18:42:48 +1000

    :art: don't use LowCoverage rule

--- src/austrakka_covid_tree/workflow/scripts/mask_lineages.py
+++ src/austrakka_covid_tree/workflow/scripts/mask_lineages.py
@@ -6,7 +6,7 @@ df = pd.read_csv(snakemake.input.nextclade_tsv, sep="\t")
 
 df.coverage = df.coverage.fillna(0)
 df.coverage = df.coverage * 100
-df.loc[df.coverage < 90, "Nextclade_pango"] = "LowCoverage"
+# df.loc[df.coverage < 90, "Nextclade_pango"] = "LowCoverage"
 
 df["qc.overallStatus"] = df["qc.overallStatus"].fillna("bad")
 df.loc[df["qc.overallStatus"] == "bad", "Nextclade_pango"] = "Unassigned"
