80dd81e940729b33ba8a3dc20365c3d19a9378bf
Wytamma Wirth <wytamma.wirth@me.com>
2024-04-23 14:35:30 +1000

    :arrow_up: update to snk_cli

--- src/austrakka_sc2_tree/cli/__init__.py
+++ src/austrakka_sc2_tree/cli/__init__.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from snk.cli import CLI
+from snk_cli import CLI
 
 from austrakka_sc2_tree.__about__ import __version__
 from austrakka_sc2_tree.cli.upload import upload
 
-austrakka_sc2_tree = CLI(pipeline_dir_path=Path(__file__).parent.parent)
+austrakka_sc2_tree = CLI(Path(__file__).parent.parent)
+austrakka_sc2_tree.workflow.name = "austrakka-sc2-tree"
 austrakka_sc2_tree.version = __version__
 austrakka_sc2_tree.register_command(upload)
